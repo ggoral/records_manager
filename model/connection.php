d3355e4e7ac4096c7a9736668a62f1aa45b4ce0b
ggoral <goral.gonzalo@gmail.com>
2013-11-27 20:58:03 -0300

    add model

---
+++ model/connection.php
@@ -0,0 +1,70 @@
+<?php
+ 
+class Conexion 
+  {
+    private $conexion;
+
+    function crear_conexion()
+      {
+        if(!isset($this->conexion)){
+          $db_host="127.0.0.1";
+          $db_user="proyecto";
+          $db_pass="proyecto";
+          $db_base="grupo56";
+          $this->conexion = new PDO("mysql:dbname=$db_base;host=$db_host",$db_user,$db_pass);
+        }
+      }
+
+    function cerrar_conexion()
+      {
+        $this->conexion = null;
+      }
+
+    function consulta($consulta, $atributos=null)
+      {
+        $this->crear_conexion();
+        try{
+          $query = $this->conexion->prepare($consulta);
+          $query->execute($atributos);
+          $resultado = $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $e){
+          $resultado = 'Error: ' . $e->getMessage();
+        }
+        $this->cerrar_conexion();
+        return $resultado;
+      }
+
+    function consulta_fetch($consulta, $atributos=null)
+      {
+        $this->crear_conexion();
+        try{
+          $query = $this->conexion->prepare($consulta);
+          $query->execute($atributos);
+          $resultado = $query->fetch(PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $e){
+          $resultado = 'Error: ' . $e->getMessage();
+        }
+        $this->cerrar_conexion();
+        return $resultado;
+      }
+
+      function consulta_row($consulta, $atributos=null)
+      {
+        $this->crear_conexion();
+        try{
+          $query = $this->conexion->prepare($consulta);
+          $query->execute($atributos);
+          $affected_rows = $query->rowCount();
+        }
+        catch(PDOException $e){
+          $affected_rows = 'Error: ' . $e->getMessage();
+        }
+        $this->cerrar_conexion();
+        return $affected_rows;
+      }
+
+  }
+
+?>
---
+++ model/expediente.php
@@ -0,0 +1,28 @@
+<?php
+
+class Expediente
+  {
+   private $id_usuario;
+   private $username;
+   private $password;
+   private $causante;
+   private $extracto;
+   private $personas; //array de personas
+   private $keywords; //array de keywords
+   private $rol;
+
+$expediente = array(
+  "id_usuario" => "",
+  "username" => "",
+  "password" => "",
+  "causante" => "",
+  "extracto" => "",
+  "personas" => "",
+  "keywords" => "",
+  "rol" => "",
+  ); 
+
+// $expediente["id_usuario"] = seter
+// $expediente["id_usuario"]
+// array_search("keyword",$expediente["keywords"]);
+?>
